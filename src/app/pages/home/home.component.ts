2d8db913d835ba7fb602a5ef2f19cd349529cce3
unknown <junaidhussain294@gmail.com>
2023-06-02 18:08:55 +0530

    data setup

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+public data="Hello world";
+public color= "text-primary";
+public data1= true;
+public data2= [
+  {
+name: 'aaaaa',
+id: 1,
+  },
+  {
+name: 'bbbbb',
+id: 2,
+  },
+{
+name: 'ccccc',
+id: 3,
+  },
+  {
+name: 'ddddd',
+id: 4,
+  },
+  {
+name: 'eeeee',
+id: 5,
+
+  },
+]
 
 }
